ff8d7d380491e353ef250c961f673d270bf70d3c
Sec1 <sec1code.git@gmail.com>
2023-12-11 21:14:32 +0100

    Updated Comments

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-/*TODO: - scale small images ----------------------DONE------------------------
-        - gucken warum die bilder nicht richtig sind (debug)
+/*TODO:
+        - Implement crossover of parents
         - always display original image
         - display text with the generation number
         - clean up the design (colour)
         - get rid of unnecessary code
         - clean up comments
         - (maybe rewrite in german)
-        - make actually good comments
-        - yeah I think that's enough!
 */
 
 public class Main {
